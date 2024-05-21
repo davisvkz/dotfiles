991aea268a91f51429b364596a8810b9ee76b8d6
davisvkz <davissviana2006@gmail.com>
2024-05-21 16:11:28 -0300

    feat(polybar): added launch script

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload BAR &
+  done
+else
+  polybar --reload BAR &
+fi
