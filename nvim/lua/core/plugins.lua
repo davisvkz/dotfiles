5d36394c023e9e57ffe19aab209dae994d2c76da
davisvkz <davissviana2006@gmail.com>
2024-05-21 16:11:28 -0300

    feat(nvim): added lazy packer manager

---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,20 @@
+package.path = package.path .. ";/home/davisvkz/.luarocks/share/lua/5.1/?/init.lua;/home/davisvkz/.luarocks/share/lua/5.1/?.lua;"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+   change_detection = {
+    enabled = true, -- automatically check for config file changes and reload the ui
+    notify = false, -- turn off notifications whenever plugin changes are made
+  },
+})
