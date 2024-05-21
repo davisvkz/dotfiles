7ccaf0a0dcf3a4fb8bff3701510f20467db1f596
davisvkz <davissviana2006@gmail.com>
2024-05-21 16:11:28 -0300

    feat(nvim): added lazygit

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,16 @@
+return {
+	'kdheepak/lazygit.nvim',
+	dependencies = {
+		'nvim-lua/plenary.nvim'
+	},
+	config = function()
+		vim.keymap.set('n', '<leader>x', ':LazyGit<CR>')
+		vim.g.lazygit_floating_window_winblend = 0
+		vim.g.lazygit_floating_window_scaling_factor = 0.9
+		vim.g.lazygit_floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' }
+		vim.g.lazygit_floating_window_use_plenary = 0
+		vim.g.lazygit_use_neovim_remote = 1
+		vim.g.lazygit_use_custom_config_file_path = 0
+		vim.g.lazygit_config_file_path = ''
+	end
+}
