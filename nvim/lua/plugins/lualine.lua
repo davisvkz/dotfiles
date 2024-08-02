79798134392c9cb10776a6a16c27eaa0cf3c7c8e
davisvkz <davissviana2006@gmail.com>
2024-08-02 20:41:41 -0300

    fix(nvim): set my lualine opts

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,44 +1,84 @@
 return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {
-		options = {
-			icons_enabled = true,
-			theme = 'auto',
-			component_separators = { left = '', right = ''},
-			section_separators = { left = '', right = ''},
-			disabled_filetypes = {
-				statusline = {},
-				winbar = {},
+	opts = function(_, opts)
+		local trouble = require("trouble")
+		local symbols = trouble.statusline({
+			mode = "lsp_document_symbols",
+			groups = {},
+			title = false,
+			filter = { range = true },
+			format = "{kind_icon}{symbol.name:Normal}",
+			-- The following line is needed to fix the background color
+			-- Set it to the lualine section you want to use
+			hl_group = "lualine_c_normal",
+		})
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = 'auto',
+				component_separators = { left = '', right = '' },
+				section_separators = { left = '', right = '' },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				}
 			},
-			ignore_focus = {},
-			always_divide_middle = true,
-			globalstatus = false,
-			refresh = {
-				statusline = 1000,
-				tabline = 1000,
-				winbar = 1000,
-			}
-		},
-		sections = {
-			lualine_a = {'mode'},
-			lualine_b = {'branch', 'diff', 'diagnostics'},
-			lualine_c = {'filename'},
-			lualine_x = {'encoding', 'fileformat', 'filetype'},
-			lualine_y = {'progress'},
-			lualine_z = {'location'}
-		},
-		inactive_sections = {
-			lualine_a = {},
-			lualine_b = {},
-			lualine_c = {'filename'},
-			lualine_x = {'location'},
-			lualine_y = {},
-			lualine_z = {}
-		},
-		tabline = {},
-		winbar = {},
-		inactive_winbar = {},
-		extensions = {}
-	}
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch', 'diff', 'diagnostics' },
+				lualine_c = { 'filename', {
+					symbols.get,
+					cond = symbols.has,
+				} },
+				lualine_x = { 'filetype' },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' }
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { 'filename' },
+				lualine_x = { 'location' },
+				lualine_y = {},
+				lualine_z = {}
+			},
+			tabline = {
+				lualine_a = { {
+					'buffers',
+					show_filename_only=true,
+					hide_filename_extension= true,
+					show_modified_status = true,
+					mode = 0,
+					right_padding=0
+				} },
+				lualine_z = { 'tabs' }
+			},
+			winbar = {
+			},
+			inactive_winbar = {},
+			extensions = {}
+		}
+		return opts
+	end,
+	config = function(_, opts)
+		local function show_macro_recording()
+			local recording_register = vim.fn.reg_recording()
+			if recording_register == "" then
+				return ""
+			else
+				return "Recording @" .. recording_register
+			end
+		end
+		table.insert(opts.sections.lualine_a, { "macro-recording", fmt = show_macro_recording })
+		require('lualine').setup(opts)
+	end
 }
