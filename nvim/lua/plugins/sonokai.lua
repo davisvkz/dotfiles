cd00c7eb4487e7086e151585fd5f4cbdea16ffbe
davisvkz <davissviana2006@gmail.com>
2024-08-02 20:37:04 -0300

    fix(nvim): enable sonokai colorscheme

--- nvim/lua/plugins/sonokai.lua
+++ nvim/lua/plugins/sonokai.lua
@@ -1,3 +1,12 @@
 return {
 	'sainnhe/sonokai',
+	config = function()
+		vim.g.sonokay_style = 'andromeda'
+		vim.cmd[[colorscheme sonokai]]
+
+		vim.cmd[[highlight Normal guibg=NONE ctermbg=NONE]]
+		vim.cmd[[highlight NormalNC guibg=NONE ctermbg=NONE]]
+		vim.cmd[[highlight NormalSB guibg=NONE ctermbg=NONE]]
+		vim.cmd[[highlight EndOfBuffer guibg=NONE ctermbg=NONE]]
+	end
 }
